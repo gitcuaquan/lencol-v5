15d9f1c0d6045b2d0f84e1130636640876e0d42e
gitcuaquan <79095757+gitcuaquan@users.noreply.github.com>
2023-02-16 16:31:39 +0700

    update

--- model/Fabricjs.ts
+++ model/Fabricjs.ts
@@ -1,22 +1,33 @@
 import { fabric } from "fabric";
-
+import { DeleteControl } from "./FabricControl";
 export default class Frabric {
   //thuộc tính classs
-  private canvas: fabric.Canvas;
-  private canvasExport: fabric.StaticCanvas;
+
+  private canvas!: fabric.Canvas;
+  private canvasExport!: fabric.StaticCanvas;
   private width: number = 800;
   private height: number = 800;
-  private image: fabric.Image;
+  private image!: fabric.Image;
   private meshName: string;
   // --- khởi tạo cần
-  constructor(canvas: HTMLCanvasElement, canvasExport: HTMLCanvasElement,meshName:string) {
-    this.meshName = meshName
+  constructor(
+    canvas: HTMLCanvasElement,
+    canvasExport: HTMLCanvasElement,
+    meshName: string
+  ) {
+    this.meshName = meshName;
     this.CustomFrabricJs();
     this.Init(canvas, canvasExport);
   }
+  /**
+   * Hàm này khởi tạo các biến Canvas và CanvasExport, đặt màu nền của
+   * Canvas và canvasexport, sau đó gọi các hàm thay đổi kích thước () và watchcanvas ().
+   * @param {HTMLCanvasElement} canvas - HTMLCanvasElement, canvasExport: HTMLCanvasElement
+   * @param {HTMLCanvasElement} canvasExport - HTMLCanvasElement - this is the canvas that I want to
+   * export to a PNG.
+   */
   private Init(canvas: HTMLCanvasElement, canvasExport: HTMLCanvasElement) {
     this.canvas = new fabric.Canvas(canvas);
-  
     this.canvasExport = new fabric.StaticCanvas(canvasExport);
     this.canvas.setBackgroundColor(
       "#ffffff",
@@ -44,32 +55,60 @@ export default class Frabric {
       borderScaleFactor: 1,
     });
   }
+  /**
+   * "Khi cửa sổ được thay đổi kích thước, khung vẽ được thay đổi kích thước để khớp với kích thước của cửa sổ."
+   *
+   * Hàm được gọi trong hàm tạo.
+   */
   private Resize() {
     const fnc = () => {
-      this.canvas.setDimensions({
-        width: this.width,
-        height: this.height,
-      });
-      this.canvasExport.setDimensions({
-        width: this.width,
-        height: this.height,
-      });
+      if (this.canvas)
+        this.canvas.setDimensions({
+          width: this.width,
+          height: this.height,
+        });
+      if (this.canvasExport)
+        this.canvasExport.setDimensions({
+          width: this.width,
+          height: this.height,
+        });
     };
     fnc();
     window.addEventListener("resize", fnc);
   }
+  /**
+   * "Nếu Canvas có chiều cao, hãy đặt hình ảnh lớp phủ thành URL và mở rộng nó lên chiều cao của
+   * Canvas."
+   *
+   * Vấn đề là hình ảnh lớp phủ không mở rộng lên chiều cao của khung vẽ
+   * @param {string} url - string - URL của hình ảnh được sử dụng làm lớp phủ.
+   */
   public SetOverlayImage(url: string) {
-    fabric.Image.fromURL(url, async (image: fabric.Image) => {
-      await this.canvas.setOverlayImage(
+    if (this.canvas.height) var height = this.canvas.height;
+    fabric.Image.fromURL(url, (image: fabric.Image) => {
+      this.canvas.setOverlayImage(
         image,
         this.canvas.renderAll.bind(this.canvas),
         {
-          scaleX: image.height ? this.canvas.height / image.height : 0,
-          scaleY: image.height ? this.canvas.height / image.height : 0,
+          scaleX: image.height ? height / image.height : 0,
+          scaleY: image.height ? height / image.height : 0,
         }
       );
     });
   }
+  /**
+   *  đang thêm một hình ảnh vào một bức tranh, sau đó nhân bản nó vào một tấm bạt khác, sau đó xuất hình thứ hai
+   * Tranh sơn dầu.
+   *
+   * Vấn đề là hình ảnh không được nhân bản vào khung vẽ thứ hai.
+   * sử dụng mã sau để sao chép hình ảnh:
+   *
+   * image.clone((img: any) => {
+   *   this.canvasExport.add(img);
+   * });
+   *
+   * @param {string} url - string - URL của hình ảnh được thêm vào
+   */
   public AddImage(url: string) {
     fabric.Image.fromURL(url, (image: fabric.Image) => {
       var newOptions = this.ResizeImage(image);
@@ -82,9 +121,9 @@ export default class Frabric {
         this.canvasExport.add(img);
       });
       this.image = image;
-      this.canvas.insertAt(image, 1);
+      this.canvas.add(image);
       this.canvas.setActiveObject(image);
-      this.ExportImage()
+      this.ExportImage();
     });
   }
   /**
@@ -103,46 +142,45 @@ export default class Frabric {
       scale,
     };
   }
+  /**
+   *Khi người dùng di chuyển, chia tỷ lệ, xoay hoặc thêm một đối tượng vào khung vẽ, sao chép hình ảnh và thêm nó vào
+   * Canvas xuất, sau đó xuất hình ảnh.
+   */
   WatchCanvas() {
     this.canvas.on("object:moving", (e: any) => {
-      this.image.clone((img: any) => {
-        this.canvasExport.remove(this.canvasExport.item(0));
-        this.canvasExport.add(img);
-        this.ExportImage();
-      });
+      this.ExportImage();
     });
     this.canvas.on("object:scaling", (e: any) => {
-      this.image.clone((img: any) => {
-        this.canvasExport.remove(this.canvasExport.item(0));
-        this.canvasExport.add(img);
-        this.ExportImage();
-      });
+      this.ExportImage();
     });
     this.canvas.on("object:rotating", (e: any) => {
-      this.image.clone((img: any) => {
-        this.canvasExport.remove(this.canvasExport.item(0));
-        this.canvasExport.add(img);
-        this.ExportImage();
-      });
+      this.ExportImage();
     });
     this.canvas.on("object:added", (e: any) => {
-      this.image.clone((img: any) => {
-        this.canvasExport.remove(this.canvasExport.item(0));
-        this.canvasExport.add(img);
-        this.ExportImage();
-      });
+      this.ExportImage();
     });
   }
+  /**
+   * "Khi người dùng nhấp vào nút Xuất, Canvas được chuyển đổi thành phần tử Canvas và được gửi ddi thông quan event của window
+   *
+   */
   async ExportImage() {
     if (this.canvasExport) {
-      window.dispatchEvent(
-        new CustomEvent("export:image", {
-          detail:{
-            meshName : this.meshName,
-            canvas: this.canvasExport.toCanvasElement()
-          },
-        })
-      );
+      this.image.clone((img: any) => {
+        const firstObject = this.canvasExport.item(0);
+        if (firstObject instanceof fabric.Object) {
+          this.canvasExport.remove(firstObject);
+        }
+        this.canvasExport.add(img);
+        window.dispatchEvent(
+          new CustomEvent("export:image", {
+            detail: {
+              meshName: this.meshName,
+              canvas: this.canvasExport.toCanvasElement(),
+            },
+          })
+        );
+      });
     }
   }
 }
